7b2204a07ac84f4c0fa2d6cc073d40a0094cbbe6
osvaldowsjr <osvaldowilliam@outlook.com>
2020-05-23 12:49:42 -0300

    Atividade Aula23/05

--- Angular/site-divulgacao-angular/src/app/curso-cadastro/curso-cadastro.component.ts
+++ Angular/site-divulgacao-angular/src/app/curso-cadastro/curso-cadastro.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter,Output} from '@angular/core';
 
 @Component({
   selector: 'app-curso-cadastro',
   templateUrl: './curso-cadastro.component.html',
   styleUrls: ['./curso-cadastro.component.css']
 })
-export class CursoCadastroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CursoCadastroComponent{
+@Output() cursoAdicionado = new EventEmitter();
+adicionar(nome,descricao){
+  const curso = {
+    nome:nome,
+    descricao:descricao
+  };
+  this.cursoAdicionado.emit(curso)
+}
 }
